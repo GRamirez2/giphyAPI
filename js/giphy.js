c52322f2ddc541efbe5ebcc6956dcd9892eda7c4
George Ramirez <george.ramirez2@gmail.com>
2016-09-06 10:28:11 -0500

    majority of the code, neeed to work on the presentation of the gifs and the on clicks

---
+++ js/giphy.js
@@ -0,0 +1,106 @@
+
+
+
+// SETUP VARIABLES
+// ==========================================================
+// user choice from input from
+var userChoice = $('#food-input').val().trim();
+var foodButtons = ['pizza', 'hamburger', 'bacon', 'cheese', 'tacos', 'bbq', 'ribs'];
+
+
+
+
+// FUNCTIONS
+// ==========================================================
+
+// This function will trigger the AJAX Call
+	// $('#foodButton').on('click', function(){
+		function getmydata (){
+
+		//search based on button data-name
+		var keyWord = $(this).attr('data-name');
+		var apiKey = "dc6zaTOxFJmzC";
+		// the API URL
+		var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+keyWord+'&api_key='+apiKey;
+
+		// ajax hits ths gify API
+		$.ajax({url: queryURL, method: 'GET'}).done(function(results) {
+			console.log(keyWord)
+			console.log(results)
+
+			for (var i = 0; i < results.data.length; i++){
+				
+				// Creating a div for all the info I need to display
+				var final = $('<div class= "panel panel-default searchBox"><div class="panel-body thumbnails">')
+				// Getting the info I need from the results
+				var rating = results.data[i].rating
+				var ratingText = $('<p>').text("Rated: " + rating);
+				final.append(ratingText);
+
+				 /*still images at 200px*/
+				var img = $("<img>").attr('src',(results).data[i].images.fixed_width_still.url);/*still*/
+				var gig = $("<img>").attr('src',(results).data[i].images.fixed_width_downsampled.url)/*gif*/
+				final.append(img);
+			// console.log((results).data[i].images.fixed_width_still.url)
+				// pushing data into the approbriate ID
+				 // $(".gifs").prepend("Rated "+rating+"<br>"+img);
+				 
+
+				 $(".gifs").prepend(final);
+			};
+
+		});
+	     	 
+	};
+// ========================================================
+// make the buttons function
+function createButtons(){ 
+
+		// Empties the button div so you don't have repeat buttons.
+		$('.buttonDiv').empty();
+
+		// Loops through the array of foodButtons
+		for (var i = 0; i < foodButtons.length; i++){
+
+			// Then dynamicaly generates buttons for each movie in the array
+		    var BTN = $('<button type="button" class="btn btn-success" id="foodButton">')
+		    // add class
+		    BTN.addClass('foodType');
+		    // add a data-name attribute , WE WILL USE THIS DATA LATER FOR THE API REQUEST
+		    BTN.attr('data-name', foodButtons[i]);
+		    // add text to the button
+		    BTN.text(foodButtons[i]);
+		    // push the buttons to the correct div on the page
+		    $('.buttonDiv').append(BTN);
+		}
+	}
+
+
+
+// ========================================================
+
+	// Generic function for displaying the movieInfo
+	$(document).on('click', '#foodButton', getmydata);
+
+// METHODS
+// ========================================================
+
+	// This function handles events where one button is clicked
+	$('#addFood').on('click', function(){
+		
+		// This line of code will grab the input from the textbox
+		var newButton = $('.form-control').val().trim();
+		
+		// The food/word from the textbox is then added to the foodButtons array
+		foodButtons.push(newButton);
+		
+		// buttons are now created from the array with new word in the array
+		createButtons();
+
+		// users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		// return false;/*This isn't working*/
+	});
+
+// ==========================================================
+
+createButtons();
